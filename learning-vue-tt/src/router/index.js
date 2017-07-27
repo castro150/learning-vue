37f2f67040bc85438ea047df20b9d720e9962098
castro150 <rafaelcarneiroget@hotmail.com>
2017-07-27 14:50:54 -0300

    Separating router from main

--- learning-vue-tt/src/router/index.js
+++ learning-vue-tt/src/router/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Home from '@/components/Home.vue'
+import TimeEntries from '@/components/TimeEntries.vue'
+import LogTime from '@/components/LogTime.vue'
 
 Vue.use(Router)
 
+// Pointing routes to the components they should use
+const routes = [{
+	path: '/home',
+	component: Home
+}, {
+	path: '/time-entries',
+	component: TimeEntries,
+	children: [{
+		path: 'log-time',
+		component: LogTime
+	}]
+}, {
+	// Any invalid route will redirect to home
+	path: '*',
+	redirect: '/home'
+}]
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+	routes,
+	mode: 'history'
 })
