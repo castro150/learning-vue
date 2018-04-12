03e9f75e37021a6ad3abf21ce3072e3d7d86b7a8
Rafael Castro <rafaelcarneiroget@hotmail.com>
2018-04-12 14:15:38 -0300

    Alura course

---
+++ alura/alurapic/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <h1 v-text="titulo"></h1>
+
+    <ul>
+      <li v-for="foto in fotos" :key="foto.titulo">
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {
+      titulo: 'Alurapic',
+      fotos: [
+        {
+          url: 'http://tudosobrecachorros.com.br/wp-content/uploads/cachorro-independente.jpg',
+          titulo: 'cachorro'
+        },
+        {
+          url: 'http://tudosobrecachorros.com.br/wp-content/uploads/cachorro-independente.jpg',
+          titulo: 'cachorrao'
+        }
+      ]
+    }
+  }
+
+}
+</script>
+
+<style>
+</style>
